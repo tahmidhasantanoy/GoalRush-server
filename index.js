243053a432c402433c7dc859a893d1392ab057c6
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-06-10 18:41:23 +0600

    JWt verification middle ware added

--- index.js
+++ index.js
@@ -9,8 +9,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  //check first authorization or not
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: error, message: "Unauthorized access" });
+  }
+
+  //Bearer token
+  const token = authorization.split(" ")[1];
+
+  //verify jwt
+  jwt.verify(token, processenv.SECRET_ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: error, message: "Unauthorized access" });
+    }
+    req.decoded = decoded  //send token data
+    next()
+  });
+};
+
 //Mongodb
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { status } = require("express/lib/response");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oc9fgut.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,11 +57,11 @@ async function run() {
       const userData = req.body;
       console.log(userData);
 
-      const token = jwt.sign(userData,process.env.SECRET_ACCESS_TOKEN,{
-        expiresIn : '5h'
-      })
+      const token = jwt.sign(userData, process.env.SECRET_ACCESS_TOKEN, {
+        expiresIn: "5h",
+      });
 
-      res.send({token})  //why obj??
+      res.send({ token }); //why obj??
     });
 
     // Send a ping to confirm a successful connection
